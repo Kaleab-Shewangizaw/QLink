4c5727a335d2cc6d92828dfca879c5b0f2599fa9
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2025-09-23 19:11:12 +0300

    create link route added

---
+++ src/app/api/link/add-link/route.tsx
@@ -0,0 +1,47 @@
+import { auth } from "@/app/lib/auth";
+import { Link } from "@/lib/models/LinkModel";
+import dbConnect from "@/lib/mongodb";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const { name } = await req.json();
+    if (name.trim().length === 0) {
+      return NextResponse.json(
+        { message: "Link name must be a non-empty string" },
+        { status: 400 }
+      );
+    }
+    await dbConnect();
+    const heradersList = await headers();
+    const session = await auth.api.getSession({ headers: heradersList });
+
+    if (!session?.user) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    // Create new link
+    const newLink = {
+      name: name.trim(),
+      owner: session.user.id,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    await Link.create(newLink);
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Link added successfully",
+        link: newLink,
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal Server Error", error },
+      { status: 500 }
+    );
+  }
+}
