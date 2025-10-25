0bacd77bc76b2b65df18463590c6b69821ce107d
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2025-10-25 23:06:52 +0300

    url fix

--- src/app/lib/auth-client.ts
+++ src/app/lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://qlink-one.vercel.app",
 });
 
 export const { signIn, signOut, signUp, useSession } = authClient;
